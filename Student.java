e0a0ad069b0a07e7da086025236923d33380698c
Shahriman <31914692+Shahriman@users.noreply.github.com>
2021-04-28 12:49:41 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication127;
+
+/**
+ *
+ * @author shahriman.said
+ */
+public class Student {
+    private static Student boy = new Student();
+    private Student(){
+        
+    }
+    
+}
